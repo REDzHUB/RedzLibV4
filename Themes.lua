13caa73f1faae68af8a6446155ab1a41d782ede7
REDzHUB <138633493+REDzHUB@users.noreply.github.com>
2023-12-30 20:29:24 -0300

    Update Themes.lua

--- Themes.lua
+++ Themes.lua
@@ -1,4 +1,20 @@
 local Themes = {
+  ["Default"] = {
+    ["Color Hub 1"] = Color3.fromRGB(25, 25, 25),
+    ["Color Hub 2"] = Color3.fromRGB(35, 35, 35),
+    ["Color Theme"] = Color3.fromRGB(30, 140, 140),
+    ["Color Text"] = Color3.fromRGB(240, 240, 240),
+    ["Color Dark Text"] = Color3.fromRGB(200, 200, 200),
+    ["Color Stroke"] = Color3.fromRGB(60, 60, 60),
+    ["Corner Radius"] = UDim.new(0, 5),
+    ["Font"] = {
+      Enum.Font.GothamBlack,
+      Enum.Font.GothamBold,
+      Enum.Font.GothamMedium
+    },
+    ["Stroke Thickness"] = 1,
+    ["Tween Style"] = Enum.EasingStyle.Quad
+  },
   ["Dark"] = {
     ["Color Hub 1"] = Color3.fromRGB(5, 5, 5),
     ["Color Hub 2"] = Color3.fromRGB(20, 20, 20),
